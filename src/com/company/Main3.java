465b6e614e794de41b1aa21d7b2d3b679bc8ab4a
Akrestis <horrorreaper@gmail.com>
2019-07-28 14:17:42 +0300

    Lesson 7

--- src/com/company/Main3.java
+++ src/com/company/Main3.java
@@ -15,12 +15,18 @@ public class Main3 {
 
         String string = "22.01.2019 19:15:00";
         Date date;
-        DateFormat format = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss");
+        DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss");
         try {
-            date = format.parse(string);
-            System.out.println(new SimpleDateFormat("dd MMM, hh:mm aa").format(date));
+            date = formatter.parse(string);
+            newFormat(date);
         } catch (ParseException e) {
             System.out.println("Wrong date format");
         }
+    }
+
+    private static void newFormat (Date date){
+        SimpleDateFormat formatter = new SimpleDateFormat("dd MMM, h:mm aa");
+        String result = formatter.format(date);
+        System.out.println(result);
     }
 }
