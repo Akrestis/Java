e93e30bf32f797c2a2ae2bdd789cd792e8869929
Akrestis <horrorreaper@gmail.com>
2019-08-12 20:02:23 +0300

    Lesson 10

--- src/com/company/Main1.java
+++ src/com/company/Main1.java
@@ -1,50 +1,62 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.HashMap;
-import java.util.Collections;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public class Main1 {
+public class Main1 extends Application {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    private static final int BOARD_WIDTH = 500;
+    private static final int BOARD_HEIGHT = 500;
 
-        //1.) Есть документ со списком URL:
-        //Вывести топ 10 доменов которые встречаются чаще всего.
-        // В документе могут встречается пустые и недопустимые строки.
+    public static void main(String[] args) {
 
-        topUrls("urls.txt");
+        //1) Нарисовать простую сцену - домик, солнце
+
+        launch(args);
     }
 
-    private static void topUrls(String fileName) throws FileNotFoundException {
-        File file = new File(fileName);
-        Scanner scanner = new Scanner(file);
-        Map<String, Integer> map = new HashMap<>();
-        while (scanner.hasNextLine() && scanner.nextLine() != null) {
-            String line = scanner.nextLine().replaceAll(" ", "") + "/";
-            int index = line.indexOf('/');
-            String url = line.substring(0, index);
-            if (map.containsKey(url)) {
-                map.put(url, map.get(url) + 1);
-            } else {
-                map.put(url, 1);
-            }
-        }
-        List<Integer> list = new ArrayList<>(map.values());
-        Collections.sort(list);
-        Collections.reverse(list);
-        list = list.subList(0, 10);
 
-        for (Integer i : list) {
-            for (Object tmp : map.keySet()) {
-                if (map.get(tmp).equals(i)) {
-                    System.out.println(tmp + "  " + map.get(tmp));
-                }
-            }
-        }
+    @Override
+
+    public void start(Stage primary) {
+        primary.setTitle("Home");
+        Canvas canvas = new Canvas();
+        canvas.setHeight(BOARD_HEIGHT);
+        canvas.setWidth(BOARD_WIDTH);
+        BorderPane group = new BorderPane(canvas);
+        Scene scene = new Scene(group);
+        primary.setScene(scene);
+        primary.show();
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        home(gc);
+    }
+
+    private void home(GraphicsContext gc) {
+
+        gc.setFill(Color.DEEPSKYBLUE);
+        gc.fillRect(0,0,BOARD_HEIGHT,BOARD_WIDTH);
+
+        gc.setFill(Color.GREEN);
+        gc.fillRect(0, 450, BOARD_HEIGHT, BOARD_WIDTH);
+
+        gc.setFill(Color.BROWN);
+        gc.fillRect(50, 350, 150, 100);
+
+        gc.setFill(Color.RED);
+        gc.fillPolygon(new double[]{35, 125, 215}, new double[]{350, 300, 350}, 3);
+
+        gc.setFill(Color.YELLOW);
+        gc.fillOval(400, -100, 200, 200);
+
+        gc.setFill(Color.WHITE);
+        gc.fillRect(65,365,40,40);
+
+        gc.setFill(Color.BURLYWOOD);
+        gc.fillRect(145,390,40,60);
     }
 }
--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -1,25 +1,80 @@
 package com.company;
 
-public class Main2 {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.util.Random;
+
+public class Main2 extends Application {
+
+    private static final int BOARD_WIDTH = 500;
+    private static final int BOARD_HEIGHT = 500;
+    private boolean closed = false;
 
     public static void main(String[] args) {
 
-        //3*) Сделать MyMap<K,V>
+        //2*) нарисовть шар который сам двигается и отбивается от краев экрана
 
-        MyMap myMap = new MyHashMap();
-        myMap.put(1, true);
-        myMap.put("2", "qwert");
-        myMap.put(0.3, null);
-        myMap.put(1, "zzz");
-        System.out.println(myMap);
+        launch(args);
+    }
 
-        System.out.println(myMap.size());
-        System.out.println(myMap.isEmpty());
-        System.out.println(myMap.containsKey(1));
-        System.out.println(myMap.get(1));
-        System.out.println(myMap.remove(1));
-        System.out.println(myMap);
-        myMap.clear();
-        System.out.println(myMap);
+
+    @Override
+
+    public void start(Stage primary) {
+        primary.setTitle("Home");
+        Canvas canvas = new Canvas();
+        canvas.setHeight(BOARD_HEIGHT);
+        canvas.setWidth(BOARD_WIDTH);
+        BorderPane group = new BorderPane(canvas);
+        Scene scene = new Scene(group);
+        primary.setScene(scene);
+        primary.show();
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        ball(gc);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        closed = true;
+    }
+
+    private void ball(GraphicsContext gc) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Random random = new Random();
+                int speedX = random.nextInt(5) + 1;
+                int speedY = random.nextInt(5) + 1;
+                double x = 1;
+                double y = 1;
+                double diameter = 50;
+                while (!closed) {
+                    gc.clearRect(0, 0, BOARD_WIDTH, BOARD_HEIGHT);
+                    gc.setFill(Color.FORESTGREEN);
+                    gc.fillOval(x, y, diameter, diameter);
+                    try {
+                        Thread.sleep(16);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (x + diameter >= BOARD_WIDTH || x <= 0) {
+                        speedX = -speedX;
+                    }
+
+                    if (y + diameter >= BOARD_HEIGHT || y <= 0) {
+                        speedY = 0 - speedY;
+                    }
+                    x += speedX;
+                    y += speedY;
+                }
+            }
+        }).start();
     }
 }
