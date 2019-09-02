758d434ad54ec879c47427c135b901eb8a5a4f6e
Akrestis <horrorreaper@gmail.com>
2019-09-02 23:46:13 +0300

    Lesson 15

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,22 @@
 package com.company;
 
-import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Person person = new Person("Alex", 22);
-        System.out.println(person);
+     //1.) Создать список случайных чисел. Используя Stream Api подсчитать среднее арифиметическое квадратов этих чисел/
 
-        setParameters(person, "Ben", 33);
-        System.out.println(person);
-    }
-
-    private static void setParameters(Person person, String name, int age) {
-        try {
-            Field fieldName = person.getClass().getDeclaredField("name");
-            fieldName.setAccessible(true);
-            fieldName.set(person, name);
-            Field fieldAge = person.getClass().getDeclaredField("age");
-            fieldAge.setAccessible(true);
-            fieldAge.set(person, age);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
+        Random random = new Random();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(random.nextInt(10) + 1);
         }
+        System.out.println(list);
+
+        System.out.println(list.stream().mapToInt(Integer::intValue).map(s->s*s).average());
     }
 }
